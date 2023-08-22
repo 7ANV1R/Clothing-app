92b3f5322b588ca7a37eab40b7c13d432ae4201f
Tanvir <ibnmizan7878@gmail.com>
2023-08-23 01:41:16 +0600

    updated search

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -1,5 +1,9 @@
+import 'package:clothing_app/models/collection_model.dart';
 import 'package:clothing_app/utils/colors.dart';
+import 'package:clothing_app/utils/mock.dart';
 import 'package:flutter/material.dart';
+
+import 'collection_detail_page.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -9,6 +13,8 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final controller = TextEditingController();
+  List<CollectionModel> data = mockData;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,20 +28,144 @@ class _SearchPageState extends State<SearchPage> {
         child: Column(
           children: [
             TextField(
+              controller: controller,
+              onChanged: searchProduct,
+              textInputAction: TextInputAction.search,
               decoration: InputDecoration(
                 hintText: 'Search',
-                prefixIcon: const Icon(Icons.search),
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: AppColors.secondaryColor,
+                ),
                 border: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: AppColors.secondaryColor,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: AppColors.secondaryColor,
+                  ),
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
             ),
-            const Expanded(
-              child: SizedBox(),
+            const SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: controller.text.isEmpty
+                  ? Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.asset(
+                            'assets/gifs/searching.gif',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        const Text('Start Searching'),
+                      ],
+                    )
+                  : data.isEmpty
+                      ? Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.asset(
+                                'assets/gifs/nodata.gif',
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              '\'${controller.text}\' not found',
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        )
+                      : ListView.separated(
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => CollectionDetailPage(collectionType: data[index]),
+                                ));
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: AppColors.tertiaryColor.withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(8),
+                                      child: Image.asset(
+                                        data[index].imagePath,
+                                        height: 1204 / 8,
+                                        width: 1000 / 8,
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            data[index].name,
+                                            style: const TextStyle(
+                                              fontSize: 20,
+                                              letterSpacing: 0.8,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          Text(
+                                            'BDT ${data[index].price}',
+                                            style: const TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                          separatorBuilder: (context, index) => const SizedBox(
+                            height: 8,
+                          ),
+                          itemCount: data.length,
+                        ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void searchProduct(String query) {
+    final results = mockData.where((element) {
+      final name = element.name.toLowerCase();
+      final searchLower = query.toLowerCase();
+      return name.contains(searchLower);
+    });
+    setState(() {
+      data = results.toList();
+    });
   }
 }
